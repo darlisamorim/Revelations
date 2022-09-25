ae74c21b38b83140d12b4e2af8a0e3565ee20f70
Darlisaa <darlisamorim@gmail.com>
2022-09-25 18:55:07 -0300

    Update new files

---
+++ src/assets/js/main.tsx
@@ -0,0 +1,20 @@
+// React Imports
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Main Import
+import Home from './page/Main/Home'
+
+// Global Style
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+      <Home />
+  </React.StrictMode>
+)
+
+
+
+
+
