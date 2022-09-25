505ec68ddab28273cc2c601f51b1fdd459b5b7f6
Darlis A. Amorim <eu@darlisalvesamorim.com>
2022-09-25 18:30:47 -0300

    first commit

---
+++ src/page/Main/Home.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import reactLogo from '../../assets/images/react.svg'
+import './Home.css'
+import { FaAngular } from "react-icons/fa";
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+        <span></span>
+        <h1>Vite + React</h1>
+        <h2>Vite + React</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      <div className="card">
+          <FaAngular />
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
