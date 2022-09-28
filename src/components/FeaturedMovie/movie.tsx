b5e6a9f6040e10693fb22165a5ee45f621bb8bb9
Darlisaa <darlisamorim@gmail.com>
2022-09-28 10:21:05 -0300

    updates

---
+++ src/components/FeaturedMovie/movie.tsx
@@ -0,0 +1,14 @@
+// Style Import
+import './read.css'
+
+// Construct
+export function Read() {
+
+    return (
+        <aside className="posts container">
+            <div className="content grid">
+                <h1>Aside</h1>
+            </div>
+        </aside>
+    )
+}
