5ad809a74bab6d837ce8319036bad223beea2da6
Darlisaa <darlisamorim@gmail.com>
2022-09-28 00:22:12 -0300

    Update component post

--- src/components/Posts/posts.tsx
+++ src/components/Posts/posts.tsx
@@ -1,6 +1,10 @@
 // Style Import
 import './posts.css'
 
+// Image Import
+import imagePost from '../../assets/images/posts.jpg'
+import imagePostTest from '../../assets/images/posts_test.jpg'
+
 // Icon's Import
 import { FaUserEdit, FaRegCalendarAlt, FaRegComments } from 'react-icons/fa';
 
@@ -13,79 +17,197 @@ export function Posts() {
                 <h1 className="fz">Titulo section H1</h1>
 
                 <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" title="" alt=""/>
+                    <a title="" href="" className="featured">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
                     <footer className="grid__infs">
-                        <div>
-                            <span>Tecnologia</span>
-                        </div>
-                        <div>
-                            <h2>Titulo article H2</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
-                            </p>
-                        </div>
-                        <div>
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Modern technology essaya our daily life easier and batter
+                            </a>
+                        </h2>
+                        <p>
                             <span><FaUserEdit />Admin</span>
                             <span><FaRegCalendarAlt />Date</span>
                             <span><FaRegComments />Comments</span>
-                        </div>
-                    </footer>
-                </article>
-                <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" width="80" title="" alt=""/>
-                    <footer className="grid__infs">
-                        <h2>Titulo article H2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
                         </p>
                     </footer>
-                </article>
-                <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" width="80" title="" alt=""/>
-                    <footer className="grid__infs">
-                        <h2>Titulo article H2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
-                        </p>
-                    </footer>
-                </article>
-                <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" width="80" title="" alt=""/>
-                    <footer className="grid__infs">
-                        <h2>Titulo article H2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
-                        </p>
-                    </footer>
-                </article>
-                <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" width="80" title="" alt=""/>
-                    <footer className="grid__infs">
-                        <h2>Titulo article H2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
-                        </p>
-                    </footer>
-                </article>
-                <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" width="80" title="" alt=""/>
-                    <footer className="grid__infs">
-                        <h2>Titulo article H2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
-                        </p>
-                    </footer>
-                </article>
-                <article className="grid__item">
-                    <img src="https://source.unsplash.com/random" width="80" title="" alt=""/>
-                    <footer className="grid__infs">
-                        <h2>Titulo article H2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nisi.
-                        </p>
-                    </footer>
-                </article>
 
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePostTest} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                            September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
+                <article className="grid__item">
+                    <a title="" href="">
+                        <img src={imagePost} title="" alt=""/>
+                    </a>
+                    <footer className="grid__infs">
+                        <a title="" href="" className="category">
+                            Technology
+                        </a>
+                        <h2>
+                            <a title="" href="">
+                                Technology at dignissimos porta perspi desktop
+                            </a>
+                        </h2>
+                        <span className="date">
+                            <FaRegCalendarAlt />
+                             September 18, 2022
+                        </span>
+                    </footer>
+                </article>
 
             </div>
         </section>
