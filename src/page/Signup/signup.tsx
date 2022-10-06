e62c6953b83622199172243dea03e155dcd44d5b
Darlis A. Amorim <eu@darlisalvesamorim.com>
2022-10-06 03:32:45 -0300

    Update new files

---
+++ src/page/Signup/signup.tsx
@@ -0,0 +1,18 @@
+// React Import
+import React from 'react'
+
+// Style Import
+import './signin.css'
+
+// Construct
+function Signin() {
+
+    return (
+        <main className="container">
+            <h1>Login</h1>
+        </main>
+    )
+
+}
+
+export default Signin
