e18652f0c7eec0fb2156705a126b73faeefee3db
Darlis A. Amorim <eu@darlisalvesamorim.com>
2022-10-06 04:17:11 -0300

    Modify Heroku

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Import Routers
-// import Header from "./templates/Header/header";
 import Routers from './routers';
-// import Footer from "./templates/Footer/footer";
 
 // Global Style
 import './style.css'
@@ -13,9 +11,7 @@ import './style.css'
 // Construct
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        {/*<Header />*/}
         <Routers />
-        {/*<Footer />*/}
     </React.StrictMode>
 )
 
